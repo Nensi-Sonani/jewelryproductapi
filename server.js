571c5950a3162001accb287b50815bf52efc2d26
Nensi-Sonani <nrsonani13@gmail.com>
2025-03-27 07:30:46 +0530

    api2

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-import { create, router as _router, defaults } from "json-server"; // importing json-server library
-const server = create();
-const router = _router("db.json");
-const middlewares = defaults();
+import jsonServer from './node_modules/json-server/node_modules/chalk/source/index.js' // importing json-server library
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
